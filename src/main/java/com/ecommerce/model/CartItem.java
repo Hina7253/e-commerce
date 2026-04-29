7c3eba72ac95caab814d953ed3d4e62b368c281b
Hina7253 <hinakumari.725394@gmail.com>
2026-04-29 21:30:15 +0530

    add CartItem.java

---
+++ src/main/java/com/ecommerce/model/CartItem.java
@@ -0,0 +1,25 @@
+package com.ecommerce.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.math.BigDecimal;
+
+@Entity
+@Data
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private Integer quantity;
+    private BigDecimal price;
+    private BigDecimal totalPrice;
+}
