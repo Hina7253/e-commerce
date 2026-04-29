b6df1455279e0d3e3a333d19ba0a70bb8d72b40c
Hina7253 <hinakumari.725394@gmail.com>
2026-04-29 21:27:27 +0530

    add orderItem.java

---
+++ src/main/java/com/ecommerce/model/OrderItem.java
@@ -0,0 +1,26 @@
+package com.ecommerce.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.math.BigDecimal;
+
+@Entity
+@Data
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private String productName;
+    private Integer quantity;
+    private BigDecimal priceAtTime;
+    private BigDecimal total;
+}
