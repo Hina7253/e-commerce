1f10619cc22761518c459e705412874674d32cf8
Hina7253 <hinakumari.725394@gmail.com>
2026-04-29 21:32:18 +0530

    add Address.java

---
+++ src/main/java/com/ecommerce/model/Address.java
@@ -0,0 +1,26 @@
+package com.ecommerce.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String fullName;
+    private String phoneNumber;
+    private String addressLine1;
+    private String addressLine2;
+    private String city;
+    private String state;
+    private String postalCode;
+    private String country;
+    private boolean isDefault;
+}
