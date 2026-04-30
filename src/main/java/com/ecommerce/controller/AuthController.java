32406804bacd0dc32e21ccfc6f58a8485b876ad1
Hina7253 <hinakumari.725394@gmail.com>
2026-04-30 22:10:40 +0530

    add Auth Controller.java

---
+++ src/main/java/com/ecommerce/controller/AuthController.java
@@ -0,0 +1,52 @@
+package com.ecommerce.controller;
+
+import com.ecommerce.dto.LoginRequest;
+import com.ecommerce.dto.LoginResponse;
+import com.ecommerce.dto.RegisterRequest;
+import com.ecommerce.model.User;
+import com.ecommerce.security.JwtUtil;
+import com.ecommerce.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@CrossOrigin(origins = "http://localhost:5173")
+public class AuthController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
+        );
+
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(request.getEmail());
+        final String jwt = jwtUtil.generateToken(userDetails.getUsername(),
+                userDetails.getAuthorities().iterator().next().getAuthority());
+
+        return ResponseEntity.ok(new LoginResponse(jwt, request.getEmail()));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> register(@RequestBody RegisterRequest request) {
+        User user = userService.register(request);
+        return ResponseEntity.ok(user);
+    }
+}
